ac5f0e0569c913bc6d6658f18475d4b06c64f49f
Daein <dnfwlxo11@naver.com>
2021-06-20 01:29:08 +0900

    add: youtube video download code

---
+++ app/ytSave.py
@@ -0,0 +1,18 @@
+from pytube import YouTube
+import hashlib
+from datetime import datetime
+
+def saveVideo(src):
+    yt = YouTube(src)
+    hash_ = hashlib.new('sha256')
+    hash_.update(str(datetime.now()).encode())
+
+    vids = yt.streams.filter(progressive=True, file_extension='mp4', res='360p').first()
+
+    savePath = 'static/'
+    vids.download(savePath, filename='newVideo_{}'.format(hash_.hexdigest()))
+
+    return hash_
+
+saveVideo('https://www.youtube.com/watch?v=o-9_r_jTxJs')
+
