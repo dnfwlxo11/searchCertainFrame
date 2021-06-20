c2a8ffa43108a11795537245f739a4afa4177667
Daein <dnfwlxo11@naver.com>
2021-06-20 23:22:07 +0900

    add: image div, api route code

--- run.py
+++ run.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template('html/index.html')
 
+@app.route('/api/startAnalyze', methods=['POST', 'GET'])
+def getSimilarPart():
+    return 'asd'
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=15000, debug=True)
